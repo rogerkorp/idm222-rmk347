9ccb56250a6b0b6ebbb95df732a709445a5a530f
rogerkorp <rmk347@drexel.edu>
2021-03-18 00:10:37 -0400

    :lipstick: Reworked Work Page UI
    There's still a few more things to be done, but now the main page takes up a bit more space.

---
+++ hamburger-index.js
@@ -0,0 +1,11 @@
+function openMenu (){
+    const menu = document.getElementById('burgerMenu');
+    const img = document.getElementById('popoutButton');
+    if (menu.style.display === 'none'){
+        menu.style.display = 'block';
+        img.src = "content/buttonx-2x.png";
+    } else {
+        menu.style.display = 'none';
+        img.src="content/buttonE-2x.png"
+    }
+}
